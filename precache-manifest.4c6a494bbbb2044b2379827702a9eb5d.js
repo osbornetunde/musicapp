0f897b245fbea003d915269735f1a4e1b01bf5b2
osbornetunde <osbornetunde@gmail.com>
2019-03-06 23:34:04 +0100

    Updates

---
+++ precache-manifest.4c6a494bbbb2044b2379827702a9eb5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "581942f6ea4da2fb7807",
+    "url": "/musicapp/static/js/runtime~main.581942f6.js"
+  },
+  {
+    "revision": "2dc8721dec0baecad920",
+    "url": "/musicapp/static/js/main.2dc8721d.chunk.js"
+  },
+  {
+    "revision": "a7911816d1bde8181468",
+    "url": "/musicapp/static/js/2.a7911816.chunk.js"
+  },
+  {
+    "revision": "2dc8721dec0baecad920",
+    "url": "/musicapp/static/css/main.c1a9e6a3.chunk.css"
+  },
+  {
+    "revision": "04a8142e03f6438f799a59c8e93696eb",
+    "url": "/musicapp/index.html"
+  }
+];
