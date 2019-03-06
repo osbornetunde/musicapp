b940da0f2f9976e19c2f3d7093a7370e4fc072ac
osbornetunde <osbornetunde@gmail.com>
2019-03-07 00:31:20 +0100

    updated CORS http to https

--- src/apis/trending.js
+++ src/apis/trending.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL: 'http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1',
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1',
     params: {
         page: '1',
         page_size: '5',
